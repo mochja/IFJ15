95e01f954af042f33c98a574d3fe0485d8806b56
Tiboris <tibor.dudlak@gmail.com>
2015-11-23 21:56:47 +0100

    daco som skusal dopici

--- ial.c
+++ ial.c
@@ -1,6 +1,27 @@
 #include "ial.h"
 
+
 int length(char* s)
 {
     return 0;
+}
+
+char* substr(char*, int, int)
+{
+
+}
+
+char* concat(char*, char*)
+{
+
+}
+
+int find(char*, char*)
+{
+
+}
+
+char* sort(char*)
+{
+
 }
