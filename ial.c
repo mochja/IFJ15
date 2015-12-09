a4fe936295e55f29ddab0e90feb6b8d7d33d6630
xkrat <krajnakmatto@gmail.com>
2015-12-09 21:34:20 +0100

    hashovacia tabulka pridana do ial.c, ial.h (poziadavok zadania)

--- ial.c
+++ ial.c
@@ -50,3 +50,115 @@ char* concat(const char *str1 , const char *str2)
 
     return result_str;
 }
+
+
+/* HASH TABLE */
+
+tTable *initHashTable(int size)
+{
+    tTable *t;
+
+    if ((t = calloc(1, sizeof(tTable) + sizeof(struct hItem) * size)) == NULL) {
+        return NULL;
+    }
+
+    t->size = size;
+
+    return t;
+}
+
+void freeHashTable(tTable *t)
+{
+    if (t == NULL) {
+        return;
+    }
+
+    for (int i = 0; i < t->size; ++i) {
+        hTabItem *freeItem;
+
+        freeItem = t->array[i];
+        while (true) {
+            if (t->array[i] == NULL || t->array[i]->next == NULL) {
+                break;
+            } else {
+                freeItem = t->array[i]->next;
+                t->array[i]->next = freeItem->next;
+                free(freeItem);
+            }
+        }
+        free(t->array[i]);
+    }
+    free(t);
+}
+
+/*
+ * Hash Index Generator
+ */
+static inline uint32_t getIndex(const char *str, int tableSize)
+{
+    unsigned int h = 0;
+    const unsigned char *p;
+    for (p = (const unsigned char *) str; *p != '\0'; p++)
+        h = 65599 * h + *p;
+    return h % tableSize;
+}
+
+hTabItem *createNewItem()
+{
+    hTabItem *newItem;
+
+    if ((newItem = calloc(1, sizeof(hTabItem))) == NULL) {
+        return NULL;
+    }
+
+    newItem->dataType = -1;
+    newItem->params = -1;
+    newItem->paramPosition = -1;
+
+    return newItem;
+}
+
+void insertHashTable(tTable *t, hTabItem *newItem)
+{
+    int index = getIndex(newItem->name, t->size);
+
+    if (t->array[index] == NULL) {
+        t->array[index] = newItem;
+    } else {
+        hTabItem *i;
+
+        i = t->array[index];
+        while (true) {
+            if (i->next == NULL) {
+                i->next = newItem;
+                break;
+            } else {
+                i = i->next;
+            }
+        }
+    }
+}
+
+hTabItem *searchItem(const tTable *t, const char *name)
+{
+    int index = getIndex(name, t->size);
+
+    if (t->array[index] == NULL) {
+        return NULL;
+    } else if (strcmp(t->array[index]->name, name) == 0) {
+        return t->array[index];
+    } else {
+        hTabItem *i;
+        i = t->array[index];
+
+        while (i != NULL) {
+            if (strcmp(i->name, name) == 0) {
+                return i;
+            } else {
+                i = i->next;
+            }
+        }
+    }
+
+    return NULL;
+}
