abb3247edb89f7632d5ee8eef77f199429765cfd
xkrat <krajnakmatto@gmail.com>
2015-12-10 01:18:28 +0100

    opravene chyby v ial (bullshit), v parseri opravena chyba by david, automaticke pretypovanie pri volani vstavanych funkcii

--- ial.h
+++ ial.h
@@ -2,13 +2,7 @@
 #define IAL_H_
 
 #include <stdlib.h>
-#include <malloc.h>
 #include <string.h>
-#include <stdbool.h>
-
-#define MAX_HTSIZE 101
-
-#define uint32_t unsigned int
 
 #define length(x) strlen(x)
 
@@ -16,37 +10,5 @@ char* substr(const char *str, int pos, int count);
 char* concat(const char *str1 , const char *str2);
 int find(char*, char*);
 char* sort(char*);
-
-struct hItem {
-    bool isFree;
-    char *key;
-    struct hItem *next;
-
-    char *name;
-    int dataType;
-
-    union {
-        int iVal;
-        double dVal;
-        char *sVal;
-    };
-
-    bool isDefined;
-    int params;
-    int paramPosition;
-} tHItem;
-
-typedef struct hItem hTabItem;
-
-typedef struct {
-    int size;
-    hTabItem *array[];
-} tTable;
-
-tTable *initHashTable(int size);
-hTabItem *createNewItem();
-hTabItem *searchItem(const tTable *t, const char *name);
-void insertHashTable(tTable *t, hTabItem *newItem);
-void freeHashTable(tTable *t);
 
 #endif  // IAL_H_
