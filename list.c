38cc8795935c40aba77b907b4f0b9b42a613eaab
Ján Mochňak <xmochn00@stud.fit.vutbr.cz>
2015-12-13 21:52:32 +0100

    pridane uvodne commenty do chybajucich suborov

--- list.c
+++ list.c
@@ -1,5 +1,17 @@
-#include "list.h"
+/**
+ * Copyright (c) 2015, Team Unknown,
+ *                     Ján Mochňak,    <xmochn00@vutbr.cz>
+ *                     Tibor Dudlák,   <xdudla00@vutbr.cz>
+ *                     Dávid Prexta,   <xprext00@vutbr.cz>
+ *                     Martin Krajňák, <xkrajn02@vutbr.cz>
+ *                     Patrik Segedy,  <xseged00@vutbr.cz>
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * license.txt file in the root directory of this source tree.
+ */
 
+#include "list.h"
 
 void listInit(tvarList *L) {
     L->First = NULL;
